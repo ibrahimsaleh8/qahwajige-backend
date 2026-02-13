e9778166ca95a01a542be326071e45808ff8f111
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-02-13 19:57:37 +0200

    edit cors

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: true,
     credentials: true,
   }),
 );
