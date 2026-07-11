0d7dacb7623173b98303df678a1272c2fb239763
ibrahimsaleh8 <ebrihm576@gmail.com>
2026-07-11 18:32:25 +0300

    add new domain to allowed-origins

--- src/utils/allowedOrigins.ts
+++ src/utils/allowedOrigins.ts
@@ -59,6 +59,6 @@ export const allowedOrigins = [
   "https://wwwqahwajiz.com",
   "https://www.wwwqahwajiz.com",
 
-  "https://قهوجيين.com",
-  "https://www.قهوجيين.com",
+  "https://xn--rgb7bncfhh.com",
+  "https://www.xn--rgb7bncfhh.com",
 ];
